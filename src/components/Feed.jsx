84a805595501c778780b1f0c4c312a90328e3735
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-06-29 16:06:21 -0300

    responsive repos feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,7 @@ import RepoCard from "./RepoCard";
 
 const Feed = ({ data }) => {
     return(
-        <div className="w-[70%] grid grid-cols-2 gap-y-[34px] gap-x-8 mt-[34px] pb-6">
+        <div className="w-[70%] grid grid-cols-1 lg:grid-cols-2 gap-y-[34px] gap-x-8 mt-[34px] pb-6">
             {
                 data.map(item => (
                     <RepoCard 
