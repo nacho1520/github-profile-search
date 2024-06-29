4147f35bfe0f9f36091010fe4e7613493a2d2f52
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-06-29 16:16:52 -0300

    profile compoenent responsive

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,12 +3,12 @@ import InfoContainer from "./InfoContainer";
 
 const Profile = ({ name, bio, avatar, followers, following, location }) => {
     return(
-        <div className="flex flex-col w-[70%] 2xl:w-[50%] relative">
+        <div className="flex flex-col w-[80%] lg:w-[70%] 2xl:w-[50%] relative">
             <img 
-                className="w-[120px] h-[120px] rounded-[20px] border-8 border-primary-bg absolute top-[-30%]"
+                className="w-[120px] h-[120px] rounded-[20px] border-8 border-primary-bg absolute top-[-20%] lg:top-[-30%]"
                 src={ avatar }
             />
-            <div className="inline-flex flex-wrap gap-5 ml-36 p-3 mb-5 self-center">
+            <div className="flex flex-col gap-y-2 lg:flex-row lg:inline-flex lg:flex-wrap lg:gap-5 ml-36 p-3 mb-5 self-center">
                     <InfoContainer 
                         label="Followers"
                         info={ followers }
